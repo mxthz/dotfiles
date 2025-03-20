4b6fa5983b1599aba9433d706b1ecd4244e85cf0
Max <max@yonoco.app>
2025-03-20 10:36:31 +0100

    ✨ Add keymap to populate `files` and `diagnotics` actions to quickfix list

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -1,6 +1,20 @@
+local actions = require("fzf-lua.actions")
+require("fzf-lua").setup({
+	actions = {
+		files = {
+			true,
+			["ctrl-q"] = actions.file_sel_to_qf,
+		},
+		diagnostics = {
+			true,
+			["ctrl-q"] = actions.file_sel_to_qf,
+		},
+	},
+})
+
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', '<leader>d', ':FzfLua diagnostics_workspace<CR>', opts)
-vim.keymap.set('n', '<leader>f', ':FzfLua files<CR>', opts)
-vim.keymap.set('n', '<leader>g', ':FzfLua grep<CR>', opts)
-vim.keymap.set('n', '<leader>F', ':FzfLua buffers<CR>', opts)
+vim.keymap.set("n", "<leader>d", ":FzfLua diagnostics_workspace<CR>", opts)
+vim.keymap.set("n", "<leader>f", ":FzfLua files<CR>", opts)
+vim.keymap.set("n", "<leader>g", ":FzfLua grep<CR>", opts)
+vim.keymap.set("n", "<leader>F", ":FzfLua buffers<CR>", opts)
