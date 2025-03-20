0c0e3045464b90653251fee1156e905a2b29925d
Max <max@yonoco.app>
2025-03-20 06:13:41 +0100

    🐛 Add `Reload` command as workaround for LSP breaking on `:e!` command

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@
 require("colorscheme")
 require("options")
 require("keymaps")
+require("scripts/comment-header")
 -- PLUGINS
 require("plugins")
 --Configs
@@ -14,3 +15,8 @@ require("config/flutter-tools")
 require("config/oil")
 
 require("nvim-autopairs").setup({})
+
+vim.api.nvim_create_user_command("Reload", function()
+	vim.cmd("e!")
+	vim.lsp.buf_attach_client(0, vim.lsp.get_active_clients()[1].id)
+end, {})
