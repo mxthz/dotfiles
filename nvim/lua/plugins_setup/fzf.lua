c04e2a50905b58e3560a00ee5b29fef9d2b08a7f
Max <max@yonoco.app>
2025-07-05 20:17:20 +0200

    🔧 (nvim:fzf): Update key mapping for :FzfLua buffers command

--- nvim/lua/plugins_setup/fzf.lua
+++ nvim/lua/plugins_setup/fzf.lua
@@ -17,4 +17,4 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>d", ":FzfLua diagnostics_workspace<CR>", opts)
 vim.keymap.set("n", "<leader>f", ":FzfLua files<CR>", opts)
 vim.keymap.set("n", "<leader>g", ":FzfLua grep<CR>", opts)
-vim.keymap.set("n", "<leader>F", ":FzfLua buffers<CR>", opts)
+vim.keymap.set("n", "<leader>b", ":FzfLua buffers<CR>", opts)
