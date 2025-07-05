16e31d802eb6f9bf4ddcb089bcc750bb04cbad27
Max <max@yonoco.app>
2025-07-05 19:52:52 +0200

    🔧 (nvim:lsp): Changed key mapping for code action command

--- nvim/lua/plugins_setup/lsp.lua
+++ nvim/lua/plugins_setup/lsp.lua
@@ -9,65 +9,65 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set("n", "gR", vim.lsp.buf.rename, bufopts)
-  vim.keymap.set("n", "gc", vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-  -- "vim.lsp.buf.formatting" was default
-  -- but flutter-tools conflicts and prompt to choose a server while formatting
-  -- so I'm running both sequencially
-  vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "gR", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	-- "vim.lsp.buf.formatting" was default
+	-- but flutter-tools conflicts and prompt to choose a server while formatting
+	-- so I'm running both sequencially
+	vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
 
-  vim.diagnostic.config({
-    virtual_text = true,
-  })
+	vim.diagnostic.config({
+		virtual_text = true,
+	})
 
-  --Inlay hints
-  vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+	--Inlay hints
+	vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 end
 
 local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
 }
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "ts_ls",
-    "jsonls",
-    "rust_analyzer",
-  },
-  handlers = {
-    -- Default handler for all servers
-    function(server)
-      require("lspconfig")[server].setup({
-        on_attach = on_attach,
-        flags = lsp_flags
-      })
-    end,
-  }
+	ensure_installed = {
+		"lua_ls",
+		"ts_ls",
+		"jsonls",
+		"rust_analyzer",
+	},
+	handlers = {
+		-- Default handler for all servers
+		function(server)
+			require("lspconfig")[server].setup({
+				on_attach = on_attach,
+				flags = lsp_flags,
+			})
+		end,
+	},
 })
 
 local module = {
-  on_attach = on_attach,
+	on_attach = on_attach,
 }
 
 return module
