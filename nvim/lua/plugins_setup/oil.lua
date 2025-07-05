02f0685eda988d728e5fb18b46bd02fdcfb5d3c0
Max <max@yonoco.app>
2025-07-05 20:15:17 +0200

    🔥 (nvim:oil): Remove <leader>b keymap

--- nvim/lua/plugins_setup/oil.lua
+++ nvim/lua/plugins_setup/oil.lua
@@ -1,6 +1,4 @@
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<leader>b", ":Oil<CR>",
-  { desc = "Open Oil floating window", silent = true, noremap = true, })
 
 require("oil").setup({
   default_file_explorer = true,
