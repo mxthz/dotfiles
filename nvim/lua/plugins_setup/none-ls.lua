b6c7ff2dd79c4f661368156b6f6bf370d65d35f4
Max <max@yonoco.app>
2025-07-17 09:01:08 +0200

    🔥 (nvim:none-ls): Remove useless code

--- nvim/lua/plugins_setup/none-ls.lua
+++ nvim/lua/plugins_setup/none-ls.lua
@@ -18,22 +18,7 @@ require("mason-null-ls").setup({
 	automatic_installation = true,
 })
 
--- Setup null-ls without the on_attach function to disable automatic format-on-save.
 require("null-ls").setup({
-	should_attach = function(bufnr)
-		-- Check if a deno.json file exists in the project root.
-		local is_deno_project =
-			require("lspconfig").util.root_pattern("deno.json", "deno.jsonc")(vim.api.nvim_buf_get_name(bufnr))
-
-		if is_deno_project then
-			-- If we are in a Deno project, DO NOT attach none-ls.
-			vim.notify("[none-ls] Skipping attach in Deno project.", vim.log.levels.INFO)
-			return false
-		end
-
-		-- Otherwise, it's safe to attach none-ls.
-		return true
-	end,
 	sources = lSsources,
 })
 
